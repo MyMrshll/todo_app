ba967e9282a77d2cc9889fd5353c822cff841591
Ferta <fertaprogram@gmail.com>
2024-12-26 16:32:33 +0700

    first commit

---
+++ controllers.js
@@ -0,0 +1,58 @@
+const fs = require("fs-extra");
+
+const getDataLocal = async (req, res) => {
+  const fileBuffer = await fs.readFile("./task.json");
+  const data = JSON.parse(fileBuffer);
+  return data;
+};
+
+const getData = async (req, res) => {
+  const data = await getDataLocal();
+  res.json(data);
+};
+
+const addData = async (req, res) => {
+  const payload = req.body;
+  const datas = await getDataLocal();
+  const newData = { id: datas.length + 1, ...payload };
+  datas.push(newData);
+  await fs.writeFile("./task.json", JSON.stringify(datas));
+  res.json({
+    message: "Data added successfully",
+    data: datas,
+  });
+};
+
+const deleteData = async (req, res) => {
+    const id = req.params.id;
+    const datas = await getDataLocal();
+    const newData = datas.filter((data) => data.id != id);
+    await fs.writeFile("./task.json", JSON.stringify(newData));
+    res.json({
+        message: "Data deleted successfully",
+        data: newData,
+    });
+}
+
+const updateData = async (req, res) => {
+    const id = req.params.id;
+    const datas = await getDataLocal();
+    const newData = datas.map((data) => {
+        if(data.id == id) {
+            data.completed = true;
+        }
+        return data;
+    })
+    await fs.writeFile("./task.json", JSON.stringify(newData));
+    res.json({
+        message: "Data updated successfully",
+        data: newData,
+    });
+};
+
+module.exports = {
+  getData,
+  addData,
+  deleteData,
+  updateData
+};
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const {getData, addData, deleteData, updateData} = require("./controllers");
+const log = require("./log");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+app.use(log)
+
+
+app.get("/", (req, res) => {
+  res.send("API TODO APP TASK");
+});
+
+app.get("/task", getData );
+
+app.post("/task", addData);
+
+app.delete("/task/:id", deleteData);
+
+app.put("/task/:id", updateData);
+
+
+app.listen(3020, () => {
+  console.log("Server is running on http://localhost:3020");
+});
